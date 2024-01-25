984b1aa001ad9c389b517f0993f8ec48f9ce1473
Faizan Saif <faizansaif4625@gmail.com>
2024-01-25 21:54:39 +0500

    bbb

--- lib/src/controllers/addincomeexpenseController.dart
+++ lib/src/controllers/addincomeexpenseController.dart
@@ -3,8 +3,53 @@ import 'package:get/get.dart';
 
 class AddIncomeExpenseController extends GetxController {
   TextEditingController dateinput = TextEditingController();
+  TextEditingController incomeSource = TextEditingController();
   TextEditingController amount = TextEditingController();
   dynamic selectedReason;
   dynamic selectedIncomeExpense;
+  RxBool isExpense = false.obs;
 
+  void save() {
+    if (dateinput.text.isEmpty) {
+      Get.snackbar('Error', 'Select Date',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red,
+          colorText: Colors.white);
+      return;
+    }
+    if (amount.text.isEmpty) {
+      Get.snackbar('Error', 'Enter Amount',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red,
+          colorText: Colors.white);
+      return;
+    }
+    if (selectedIncomeExpense == null) {
+      Get.snackbar('Error', 'Select Type Income or Expense',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red,
+          colorText: Colors.white);
+      return;
+    }
+
+    if (selectedIncomeExpense == 'Expense') {
+      if (selectedReason == null) {
+        Get.snackbar('Error', 'Select Expense reason',
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: Colors.red,
+            colorText: Colors.white);
+        return;
+      }
+    }
+
+    if (selectedIncomeExpense == 'Income') {
+      if (incomeSource.text.isEmpty) {
+        Get.snackbar('Error', 'Please Tell your source of income',
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: Colors.red,
+            colorText: Colors.white);
+        return;
+      }
+    }
+  }
 }
--- lib/src/ui/view/addincomeexpense.dart
+++ lib/src/ui/view/addincomeexpense.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hbb/src/controllers/addincomeexpenseController.dart';
 import 'package:hbb/src/ui/widgets/commonClasses.dart';
+import 'package:hbb/src/utils/uidata/text_styles.dart';
 import 'package:intl/intl.dart';
 
 class AddIncomeexpense extends StatelessWidget {
@@ -14,6 +15,11 @@ class AddIncomeexpense extends StatelessWidget {
         body: Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
+        Text(
+          'Add Income Expense',
+          style: UIDataTextStyles.HeadingStyle,
+        ),
+        // DateField
         SizedBox(
             width: Get.width / 1.09,
             child: TextFormField(
@@ -43,7 +49,7 @@ class AddIncomeexpense extends StatelessWidget {
 
                 if (pickedDate != null) {
                   String formattedDate =
-                      DateFormat('EEE dd MMM yyyy hh:mm a').format(pickedDate);
+                      DateFormat('yyyy-MM-dd').format(pickedDate);
 
                   _.dateinput.text = formattedDate;
                 } else {
@@ -51,6 +57,7 @@ class AddIncomeexpense extends StatelessWidget {
                 }
               },
             )),
+        // Amount TextField
         GoalsTextField(
           width: Get.width / 1.09,
           controller: _.amount,
@@ -58,6 +65,7 @@ class AddIncomeexpense extends StatelessWidget {
           heading: 'Add Amount',
           readonly: false,
         ).marginOnly(left: Get.width * 0.02),
+        // Select Type Income or Expense
         Container(
           width: Get.width / 1.09,
           height: Get.height * .045,
@@ -103,132 +111,221 @@ class AddIncomeexpense extends StatelessWidget {
               ],
               onChanged: (value) {
                 _.selectedIncomeExpense = value;
+                if (_.selectedIncomeExpense == 'Expense') {
+                  _.isExpense.value = true;
+                  print(_.selectedIncomeExpense);
+                  print(_.isExpense.isTrue);
+                } else {
+                  _.isExpense.value = false;
+                }
               }),
         ),
-        Container(
-          width: Get.width / 1.09,
-          height: Get.height * .045,
-          child: DropdownButtonFormField(
-              style: TextStyle(
-                fontSize: Get.width * .025,
-                color: Colors.black,
-              ),
-              decoration: InputDecoration(
-                hintText: "Select Reason",
-                hintStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: Get.width * .025,
-                ),
-                contentPadding: EdgeInsets.symmetric(
-                    vertical: Get.height * .01, horizontal: 10),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5),
-                  borderSide: BorderSide(color: Colors.black, width: 1),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5),
-                  borderSide: BorderSide(color: Colors.black, width: 1),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  gapPadding: 0.0,
-                  borderRadius: BorderRadius.circular(5),
-                  borderSide: BorderSide(color: Colors.black, width: 1),
-                ),
-              ),
-              iconEnabledColor: Colors.black,
-              iconSize: Get.width * .04,
-              value: _.selectedReason,
-              items: const [
-                DropdownMenuItem<String>(
-                  value: 'Advertising',
-                  child: Text('Advertising'),
-                ),
-                DropdownMenuItem<String>(
-                  value: 'Air Fare',
-                  child: Text('Air Fare'),
-                ),
-                DropdownMenuItem<String>(
-                  value: 'Bank Charges',
-                  child: Text('Bank Charges'),
-                ),DropdownMenuItem<String>(
-                  value: 'Books, publications',
-                  child: Text('Books, publications'),
-                ),DropdownMenuItem<String>(
-                  value: 'Bus and Taxi fares',
-                  child: Text('Bus and Taxi fares'),
-                ),DropdownMenuItem<String>(
-                  value: 'Business Association',
-                  child: Text('Business Association'),
-                ),DropdownMenuItem<String>(
-                  value: 'Business Opportunity Meeting',
-                  child: Text('Business Opportunity Meeting'),
-                ),DropdownMenuItem<String>(
-                  value: 'Cell Phone',
-                  child: Text('Cell Phone'),
-                ),DropdownMenuItem<String>(
-                  value: 'Computers',
-                  child: Text('Computers'),
-                ),DropdownMenuItem<String>(
-                  value: 'Entertainment',
-                  child: Text('Entertainment'),
-                ),DropdownMenuItem<String>(
-                  value: 'Home Biz Books Free',
-                  child: Text('Home Biz Books Free'),
-                ),DropdownMenuItem<String>(
-                  value: 'Home Office',
-                  child: Text('Home Office'),
-                ),DropdownMenuItem<String>(
-                  value: 'Home Office Maintainence',
-                  child: Text('Home Office Maintainence'),
-                ),DropdownMenuItem<String>(
-                  value: 'Home Office Suppliers',
-                  child: Text('Home Office Suppliers'),
-                ),DropdownMenuItem<String>(
-                  value: 'Home Office Utilities',
-                  child: Text('Home Office Utilities'),
-                ),DropdownMenuItem<String>(
-                  value: 'Home Phone',
-                  child: Text('Home Phone'),
-                ),DropdownMenuItem<String>(
-                  value: 'Insurance',
-                  child: Text('Insurance'),
-                ),DropdownMenuItem<String>(
-                  value: 'Internet Access',
-                  child: Text('Internet Access'),
-                ),DropdownMenuItem<String>(
-                  value: 'Laundary Services',
-                  child: Text('Laundary Services'),
-                ),DropdownMenuItem<String>(
-                  value: 'Leagal Fees',
-                  child: Text('Leagal Fees'),
-                ),
-                DropdownMenuItem<String>(
-                  value: 'Lodging',
-                  child: Text('Lodging'),
-                ),DropdownMenuItem<String>(
-                  value: 'Marketing Supplies',
-                  child: Text('Marketing Supplies'),
-                ),DropdownMenuItem<String>(
-                  value: 'Leagal Fees',
-                  child: Text('Leagal Fees'),
-                ),DropdownMenuItem<String>(
-                  value: 'Leagal Fees',
-                  child: Text('Leagal Fees'),
-                ),DropdownMenuItem<String>(
-                  value: 'Leagal Fees',
-                  child: Text('Leagal Fees'),
-                ),DropdownMenuItem<String>(
-                  value: 'Leagal Fees',
-                  child: Text('Leagal Fees'),
-                ),DropdownMenuItem<String>(
-                  value: 'Leagal Fees',
-                  child: Text('Leagal Fees'),
-                ),
-              ],
-              onChanged: (value) {
-                _.selectedReason = value;
-              }),
+
+        // Select Reason
+
+        Obx(
+          () => _.isExpense.value
+              ? Container(
+                  width: Get.width / 1.09,
+                  height: Get.height * .045,
+                  child: DropdownButtonFormField(
+                      style: TextStyle(
+                        fontSize: Get.width * .025,
+                        color: Colors.black,
+                      ),
+                      decoration: InputDecoration(
+                        hintText: "Select Reason",
+                        hintStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: Get.width * .025,
+                        ),
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: Get.height * .01, horizontal: 10),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                          borderSide: BorderSide(color: Colors.black, width: 1),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                          borderSide: BorderSide(color: Colors.black, width: 1),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          gapPadding: 0.0,
+                          borderRadius: BorderRadius.circular(5),
+                          borderSide: BorderSide(color: Colors.black, width: 1),
+                        ),
+                      ),
+                      iconEnabledColor: Colors.black,
+                      iconSize: Get.width * .04,
+                      value: _.selectedReason,
+                      items: const [
+                        DropdownMenuItem<String>(
+                          value: 'Advertising',
+                          child: Text('Advertising'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Air Fare',
+                          child: Text('Air Fare'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Bank Charges',
+                          child: Text('Bank Charges'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Books, publications',
+                          child: Text('Books, publications'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Bus and Taxi fares',
+                          child: Text('Bus and Taxi fares'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Business Association',
+                          child: Text('Business Association'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Business Opportunity Meeting',
+                          child: Text('Business Opportunity Meeting'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Cell Phone',
+                          child: Text('Cell Phone'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Computers',
+                          child: Text('Computers'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Entertainment',
+                          child: Text('Entertainment'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Home Biz Books Free',
+                          child: Text('Home Biz Books Free'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Home Office',
+                          child: Text('Home Office'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Home Office Maintainence',
+                          child: Text('Home Office Maintainence'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Home Office Suppliers',
+                          child: Text('Home Office Suppliers'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Home Office Utilities',
+                          child: Text('Home Office Utilities'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Home Phone',
+                          child: Text('Home Phone'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Insurance',
+                          child: Text('Insurance'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Internet Access',
+                          child: Text('Internet Access'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Laundary Services',
+                          child: Text('Laundary Services'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Leagal Fees',
+                          child: Text('Leagal Fees'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Lodging',
+                          child: Text('Lodging'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Marketing Supplies',
+                          child: Text('Marketing Supplies'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Meals',
+                          child: Text('Meals'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Membership Fees',
+                          child: Text('Membership Fees'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Mileage',
+                          child: Text('Mileage'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Parking',
+                          child: Text('Parking'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Postage',
+                          child: Text('Postage'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Prizes, Awards',
+                          child: Text('Prizes, Awards'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Tolls',
+                          child: Text('Tolls'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Other',
+                          child: Text('Other'),
+                        ),
+                      ],
+                      onChanged: (value) {
+                        _.selectedReason = value;
+                      }),
+                )
+              : GoalsTextField(
+                  width: Get.width / 1.09,
+                  controller: _.incomeSource,
+                  hintText: '',
+                  heading: 'Income Source',
+                  readonly: false,
+                ).marginOnly(left: Get.width * 0.02),
         ),
+
+// Buttons
+        Row(
+          children: [
+            OutlineButton(
+              ontap: () {
+                Get.back();
+              },
+              border: Border.all(
+                width: 2,
+                color: Colors.red,
+              ),
+              width: Get.width * 0.2,
+              color: Colors.red,
+              child: Text(
+                'Cancel',
+                style: TextStyle(color: Colors.red, fontSize: Get.width * 0.03),
+              ),
+            ),
+            Spacer(),
+            FillButton(
+              ontap: () {
+                _.save();
+              },
+              width: Get.width * 0.2,
+              color: Colors.red,
+              child: Text(
+                'Save',
+                style:
+                    TextStyle(color: Colors.white, fontSize: Get.width * 0.03),
+              ),
+            )
+          ],
+        ).marginSymmetric(horizontal: Get.width * 0.04)
       ],
     ));
   }
